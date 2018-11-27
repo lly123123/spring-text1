ea7d02658777a6e3e1c7d484cb8410b7dbdd92d3
lly123123 <1023442401@qq.com>
2018-11-27 09:12:44 +0800

    111

---
+++ src/main/java/comjk/SpringText1Application.java
@@ -0,0 +1,12 @@
+package comjk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringText1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringText1Application.class, args);
+	}
+}
